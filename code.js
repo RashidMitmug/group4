50162a23943aade0e57cde86a0b0f1ead49fe197
Valrii <143600580+RashidMitmug@users.noreply.github.com>
2024-05-28 19:28:29 +0800

    Add files via upload

--- code.js
+++ code.js
@@ -6,16 +6,39 @@ var cnum;
 var reason;
 var sex;
 
-function submitting(){
+function submitting() {
+    //getting value
     fname = document.getElementById("fname").value;
     lname = document.getElementById("lname").value;
     email = document.getElementById("email").value;
     pword = document.getElementById("pword").value;
     cnum = document.getElementById("cnum").value;
     reason = document.getElementById("reason").value;
-    sex = document.getElementsByName("opt").value;
-    alert(sex);
-   
-    
-    window.location.href="secondpage.html";
+    sex = document.querySelector('input[name="opt"]:checked').value;
+
+    //storing in localStorage
+    localStorage.setItem("fname", fname);
+    localStorage.setItem("lname", lname);
+    localStorage.setItem("email", email);
+    localStorage.setItem("pword", pword);
+    localStorage.setItem("cnum", cnum);
+    localStorage.setItem("reason", reason);
+    localStorage.setItem("opt", sex);
+
+    //open the second page
+    window.location.href = "secondpage.html";
 }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let firstnamedata = localStorage.getItem("fname");
+    let lastnamedata = localStorage.getItem("lname");
+    let emaildata = localStorage.getItem("email");
+    let cnumdata = localStorage.getItem("cnum");
+    let reasondata = localStorage.getItem("reason");
+
+    if (firstnamedata) document.getElementById("whatfname").innerText = firstnamedata;
+    if (lastnamedata) document.getElementById("whatlname").innerText = lastnamedata;
+    if (emaildata) document.getElementById("whatemail").innerText = emaildata;
+    if (cnumdata) document.getElementById("whatcnum").innerText = cnumdata;
+    if (reasondata) document.getElementById("tellmewhy").innerText = reasondata;
+});
